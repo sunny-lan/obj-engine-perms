6eeea5b8c29820f07daff23007fad0506ffbbda4
Sunny <sunny.lan.coder@gmail.com>
2017-09-08 14:09:04 -0400

    Added Config section to readme, added NVEO to publicly availible modules

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const Engine=require('./engine');
-const PermErr=require('./PermissionError');
-const crud=require('./crudPerms');
-const nveo=require('./')
+const Engine = require('./engine');
+const PermErr = require('./PermissionError');
+const crud = require('./crudPerms');
+const nveo = require('./')
 
-module.exports={
+module.exports = {
     ObjPermsEngine: Engine,
     PermissionError: PermErr,
     CRUDPerms: crud,
-
+    NVEOPerms: nveo,
 }
